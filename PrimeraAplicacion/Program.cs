83b206f178b211aca274223440c877d3385ae78f
Ángel De La Cruz <8610043+angeldelacruzdev@users.noreply.github.com>
2022-02-08 14:37:45 -0400

    first commit

---
+++ PrimeraAplicacion/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace PrimeraAplicacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Introduce un valor númerico");
+
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduce un segundo valor númerico");
+
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("El resultado es: " + (num1 + num2));
+                    
+        }
+    }
+}
